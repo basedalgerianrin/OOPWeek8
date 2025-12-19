a9b2e492aad15f9b5656c0b99e856b55c0c148a7
SamiHammar-STUDENT <awesomedude597sami@gmail.com>
2025-12-19 17:22:12 +0000

    Second Commit

---
+++ src/main/java/atu/ie/BankAccount.java
@@ -0,0 +1,28 @@
+package atu.ie;
+
+public class BankAccount {
+    private String accNo;
+    private String name;
+    private double balance;
+
+    public BankAccount(String accNo, String name, double balance) {
+        if (balance <= 0) {
+            throw new IllegalArgumentException("Balance must be greater than 0.");
+        }
+        this.accNo = accNo;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getAccNo() {
+        return accNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
